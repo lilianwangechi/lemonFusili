e4ae5ce3fb2d1c68df11d2ad7eb0e38f2cb0580f
lilian <lilian.wangechi@student.moringaschool.com>
2022-08-31 04:38:29 +0300

    made a fetch request,issue arising is that the images can not be displayed because they are comimg from other websites which are no longer existing/deny permission

--- app/index.js
+++ app/index.js
@@ -1,11 +1,20 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+
+getRecipes()
+renderFoodCard()
+
+})
+
 //render the food recipes
 //create dom elements
-const renderFoodCard = (cardData)=>{
+
+function renderFoodCard(cardData){
   const li = document.createElement('li')
-  const recipeName = document.createElement('h3')
+  const h3 = document.createElement('h3')
   const img = document.createElement('img')
-  const description = document.createElement('p')
-  const pAuthor = document.createElement('h4')
+  const p = document.createElement('p')
+  const h4 = document.createElement('h4')
   const ingredients = document.createElement('p')
   const method = document.createElement('p')
   const btn = document.createElement('button')
@@ -18,7 +27,16 @@ p.textContent = cardData.Description
 h4.textContent = cardData.Author
 p.textContent = cardData.Ingredients
 p.textContent = cardData.Method//own rray
+btn.textContent ='Delete'
 
-li.append(recipeName,img,description,pAuthor,ingredients,method,btn)
+li.append(h3,img,p,h4,ingredients,method,btn)
 document.querySelector('#food-list').append(li)
+}
+
+function getRecipes(){
+  fetch('http://localhost:3000/recipes')
+  .then(res=> res.json())
+  .then(recipes=>recipes.forEach(renderFoodCard)
+ 
+  )
 }
